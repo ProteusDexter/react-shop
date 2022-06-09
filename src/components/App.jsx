05755e0e80dd3ca5727a28bad8ee1aa06782aa32
ProteusDexter <morenobello@gmail.com>
2022-06-09 12:04:35 -0500

    template para la configuracion de css

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -7,9 +7,10 @@ const App = () => {
            // ambos elementos deben estar anidados en un elemento
            // puede ser otro div o como en este caso el parrafo
            // esta anidaddo en un titulo 
-            
+            <div>
                 <h1>Hola Mundo!<p> parrafo anidado en un h1</p></h1>
-                        
+                <p> le dimos estilo al background en color rojo y al texto en negro</p>
+            </div>            
     );
 }
 
