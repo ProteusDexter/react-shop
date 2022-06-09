d9acbfea94210b3049c11158766fc695bc22d2ce
ProteusDexter <morenobello@gmail.com>
2022-06-08 20:25:05 -0500

    configuracion de babel y webpack.config y scripts en package.json

---
+++ components/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const App = () => {
+    return (
+
+           // para que funcione es necesario que 
+           // ambos elementos deben estar anidados en un elemento
+           // puede ser otro div o como en este caso el parrafo
+           // esta anidaddo en un titulo 
+            
+                <h1>Hola Mundo!<p> parrafo anidado en un h1</p></h1>
+                        
+    );
+}
+
+export default App;
